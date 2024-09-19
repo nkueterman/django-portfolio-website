71d9d19fbfb877a1cf88a0cafd652a5d5f10a761
Nathan Kueterman <nkueterman@gmail.com>
2024-09-19 10:57:25 -0400

    project updates and render config

--- nk_portfolio/wsgi.py
+++ nk_portfolio/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Django_Portfilio_MohammadBurhan project.
+WSGI config for nk_portfolio project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_Portfilio_MohammadBurhan.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nk_portfolio.settings')
 
 application = get_wsgi_application()
