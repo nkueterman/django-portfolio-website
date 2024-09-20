89574f923b70cc44801495927260fa7ad449a48e
Nathan Kueterman <nkueterman@gmail.com>
2024-09-20 10:59:32 -0400

    grab key from env in settings.py

--- nk_portfolio/settings.py
+++ nk_portfolio/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-85t%)xy5tujn&^s3t2(9$c261e^%o)_9d(sk0d78ku#i^vxhj0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
